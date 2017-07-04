7dea7982085dfdcdfaedfade65a646fe33193366
nitu singh <nitu.sin122@gmail.com>
2017-07-04 16:27:31 +0530

    Angular 2 - Component Assign5.1 files uploaded.
    Angular 2 - Component Assign5.1 files uploaded.

---
+++ src/app/RootComponent.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app-component',
+    template: '<h2>MY FIRST OWN COMPONENT.</h2>'
+})
+
+export class RootComponent {
+    title = 'First component';
+}
+
+
